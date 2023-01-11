01af3468eda772c4b5ed7a594d18f2862fb768e9
Estrella <110181623+esordone@users.noreply.github.com>
2023-01-11 11:40:39 +0100

    Update do_op.c

--- exam rank 02/level 2/do_op/do_op.c
+++ exam rank 02/level 2/do_op/do_op.c
@@ -19,7 +19,7 @@ int main (int argc, char **argv)
 {
   if (argc == 4)
   {
-    if (argv[2][0] == '+')
+                if (argv[2][0] == '+')
 			printf("%d", (atoi(argv[1]) + atoi(argv[3])));
 		if (argv[2][0] == '-')
 			printf("%d", (atoi(argv[1]) - atoi(argv[3])));
